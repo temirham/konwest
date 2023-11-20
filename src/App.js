83e83cda517574be489e87a1a816063dbaf36b4e
temirham <temirhamgvhdd@gmail.com>
2023-11-21 02:38:07 +0300

    mobile2.6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from 'react';
 import './App.css';
 import TradingViewWidget from './components/TradingViewWidget';
@@ -8,28 +7,46 @@ import ServicesSection from './components/ServicesSection';
 import AboutSection from './components/AboutSection';
 import ReviewsSection from './components/ReviewsSection';
 import CompaniesSection from './components/CompaniesSection';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 const App = () => {
   const [currentSection, setCurrentSection] = useState('services');
+  const [isOpen, setIsOpen] = useState(false);
   const handleNavClick = (section) => {
     setCurrentSection(section);
   };
+
   return (
     <div className="consulting-app">
       <header className="consulting-header">
       </header>
-      <nav className="consulting-nav">
-        <a href="#about" onClick={() => handleNavClick('about')}>О нас</a>
-        <a href="#stock" onClick={() => handleNavClick('stock')}>Текущий курс акций и валют</a>
-        <a href="#services" onClick={() => handleNavClick('services')}>Наши услуги</a>
-        <a href="#companies" onClick={() => handleNavClick('companies')}>Компании-партнеры</a>
-        <a href="#reviews" onClick={() => handleNavClick('reviews')}>Что о нас говорят</a>
-      </nav>
+      <Navbar bg="light" expand="lg" className="consulting-navbar consulting-nav">
+      <Container fluid>
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+      >
+      </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="consulting-nav">
+            <Nav.Link className="consulting-nav a" href="#about" onClick={() => handleNavClick('about')}>О нас</Nav.Link>
+            <Nav.Link className="consulting-nav a" href="#stock" onClick={() => handleNavClick('stock')}>Текущий курс акций и валют</Nav.Link>
+            <Nav.Link className="consulting-nav a" href="#services" onClick={() => handleNavClick('services')}>Наши услуги</Nav.Link>
+            <Nav.Link className="consulting-nav a" href="#companies" onClick={() => handleNavClick('companies')}>Компании-партнеры</Nav.Link>
+            <Nav.Link className="consulting-nav a" href="#reviews" onClick={() => handleNavClick('reviews')}>Что о нас говорят</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+
       {currentSection === 'about' && <AboutSection />}
-      {currentSection === 'stock' && <div style={{marginTop: '10vh', alignContent: 'center', height: '50vh', marginLeft: '1.5vh', marginRight: '1.5vh'}}><TradingViewWidget/></div>}
+      {currentSection === 'stock' && <div style={{ marginTop: '10vh', alignContent: 'center', height: '50vh', marginLeft: '1.5vh', marginRight: '1.5vh' }}><TradingViewWidget /></div>}
       {currentSection === 'services' && <ServicesSection />}
-      {currentSection === 'companies' && <CompaniesSection />} {/* Добавлен раздел компаний */}
+      {currentSection === 'companies' && <CompaniesSection />}
       {currentSection === 'reviews' && <ReviewsSection />}
+
       <footer className="consulting-footer">
         <ContactSection />
         <p className="footer-column">&copy; 2023 Общество с ограниченной ответственностью «КОНКВЕСТ». Все права защищены.</p>
